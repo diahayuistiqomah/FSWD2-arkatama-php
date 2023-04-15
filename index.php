70c3fd45a240cf42fe45997fbb18900f4969e049
diahayuistiqomah <126170779+diahayuistiqomah@users.noreply.github.com>
2023-04-15 11:01:51 +0700

    biodata

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Biodata Diri</title>
+    <link rel="stylesheet" type="text/css" href="./style/style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Biodata Diri</h1>
+        <div class="biodata">
+            <img src="./assets/profil.jpg" alt="Foto Diri">
+            <ul>
+                <li><strong>Nama:</strong> Diah ayu istiqomah</li>
+                <li><strong>TTL:</strong> Pati, 4 Januari 2000</li>
+                <li><strong>Jenis Kelamin:</strong> Wanita</li>
+                <li><strong>Alamat Lengkap:</strong> Ds tambakromo rt 06 rw 02 kec tambakromo kab pati</li>
+                <li><strong>Riwayat Pendidikan:</strong> Sma PGRI 1 pati, Universitas Muria Kudus</li>
+                <li><strong>Hobby:</strong> Memasak, Rebahan</li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
